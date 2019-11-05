c2660fd40f73cfa887a96cecfcc354df4145a560
Paul Jena <jena.paul@gmx.de>
2019-11-05 10:20:18 +0100

    AAAAA

--- src/client.c
+++ src/client.c
@@ -16,19 +16,6 @@ int main (int argc, char **argv)
     int size;
     ifdata_t **p;
 
-    printf("%s\n", data->name);
-
-
-    for(int i = 0; data->interfaces[i] != NULL; ++i)
-    {
-        if (data->interfaces[i]->name)
-             printf("%s ", data->interfaces[i]->name);
-        if(data->interfaces[i]->physical)
-            printf("%s ", data->interfaces[i]->physical);
-        if(data->interfaces[i]->ipv4)
-            printf("%s\n", data->interfaces[i]->ipv4);
-    }
-
     if ((sock = connectto(NULL, 6661)) < 0)
     {
        printf("Verbindung mit dem Server konnte nicht aufgenommen werden\n");
